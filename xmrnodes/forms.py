27ead93038415e91ff264c924a486c01994bbb1d
Samuel Safahi <samsafahi@gmail.com>
2022-04-25 16:10:01 -0700

    add

--- xmrnodes/forms.py
+++ xmrnodes/forms.py
@@ -4,4 +4,4 @@ from wtforms.validators import DataRequired
 
 
 class SubmitNode(FlaskForm):
-    node_url = StringField('', validators=[DataRequired()], render_kw={"placeholder": "Node URL (http://xxx.tld:18081)"})
+    node_url = StringField('', validators=[DataRequired()], render_kw={"placeholder": "Node URL (http://xxx.tld:28081)"})
--- xmrnodes/helpers.py
+++ xmrnodes/helpers.py
@@ -26,14 +26,14 @@ def make_request(url: str, path="/get_info", data=None):
 def determine_crypto(url):
     data = {"method": "get_block_header_by_height", "params": {"height": 0}}
     hashes = {
-        "monero": [
-            "418015bb9ae982a1975da7d79277c2705727a56894ba0fb246adaabb1f4632e3", #mainnet
-            "48ca7cd3c8de5b6a4d53d2861fbdaedca141553559f9be9520068053cda8430b", #testnet
-            "76ee3cc98646292206cd3e86f74d88b4dcc1d937088645e9b0cbca84b7ce74eb"  #stagenet
+        # "monero": [
+        #    "418015bb9ae982a1975da7d79277c2705727a56894ba0fb246adaabb1f4632e3", #mainnet
+        #    "48ca7cd3c8de5b6a4d53d2861fbdaedca141553559f9be9520068053cda8430b", #testnet
+        #    "76ee3cc98646292206cd3e86f74d88b4dcc1d937088645e9b0cbca84b7ce74eb"  #stagenet
         ],
-        "wownero": [
-            "a3fd635dd5cb55700317783469ba749b5259f0eeac2420ab2c27eb3ff5ffdc5c", #mainnet
-            "d81a24c7aad4628e5c9129f8f2ec85888885b28cf468597a9762c3945e9f29aa", #testnet
+    #    "wownero": [
+    #        "a3fd635dd5cb55700317783469ba749b5259f0eeac2420ab2c27eb3ff5ffdc5c", #mainnet
+        #    "d81a24c7aad4628e5c9129f8f2ec85888885b28cf468597a9762c3945e9f29aa", #testnet
         ]
     }
     try:
